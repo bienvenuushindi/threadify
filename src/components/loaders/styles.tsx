a928868897a2e9ff6ee467ba739219e133cfbadc
usbbush <jeanbienvenusb@gmail.com>
2023-02-22 17:29:44 +0200

    Design numbering style loaders

---
+++ src/components/loaders/styles.tsx
@@ -0,0 +1,94 @@
+export function defaultStyle() {
+  return (
+    <div className="animate-pulse flex border p-3 shadow flex-col ">
+
+      <div className="space-y-3">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="h-5 bg-slate-200 rounded-full w-11/12 col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export function styleOne() {
+  return (
+    <div className="animate-pulse  border p-3 shadow flex-col ">
+
+      <div className="space-y-3">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex align-baseline w-100">
+            <div className="w-1/12 ">1)</div>
+            <div className="h-5 bg-slate-200 rounded-full w-11/12 col-span-1"/>
+          </div>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export function styleTwo() {
+  return (
+    <div className="animate-pulse flex border p-3 shadow flex-col ">
+
+      <div className="space-y-3">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex align-baseline w-100">
+            <div className="w-1/12 ">1)</div>
+            <div className="h-5 bg-slate-200 rounded-full w-11/12 col-span-1"/>
+          </div>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export function styleThree() {
+  return (
+    <div className="animate-pulse flex  border p-3 shadow flex-col ">
+
+      <div className="space-y-3">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex align-baseline w-100">
+            <div className="w-1/12 ">1)</div>
+            <div className="h-5 bg-slate-200 rounded-full w-11/12 col-span-1"/>
+          </div>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export function styleFour() {
+  return (
+
+    <div className="animate-pulse flex  border p-3 shadow flex-col ">
+
+      <div className="space-y-3">
+        <div className="grid grid-cols-1 gap-4">
+          <div className="flex align-baseline w-100">
+            <div className="w-1/12 ">1)</div>
+            <div className="h-5 bg-slate-200 rounded-full w-11/12 col-span-1"/>
+          </div>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+          <div className="h-5 bg-slate-200 rounded-full col-span-1"/>
+        </div>
+      </div>
+
+    </div>
+
+  );
+}
+
