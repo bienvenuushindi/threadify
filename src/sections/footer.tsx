e3385230eea847c681cfd6ae65a57885fdd3c374
usbbush <jeanbienvenusb@gmail.com>
2023-02-25 00:47:23 +0200

    Update design

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -4,9 +4,7 @@ import SocialMedia from '../components/socialMedia';
 function Footer() {
   return (
     <footer className="fixed bottom-0 h-8 bg-white  w-full text-center border">
-      <ul>
         <SocialMedia customClasses='flex justify-center gap-4 pt-1' showIconText={false}/>
-      </ul>
     </footer>
   )
 }
