e338f77bc974540f866a4174d2b8a34259ce01f4
usbbush <jeanbienvenusb@gmail.com>
2023-02-24 11:43:05 +0200

    Update function logic

--- src/utils/thread.ts
+++ src/utils/thread.ts
@@ -2,6 +2,7 @@ import {Numbering, Position} from './numbering';
 
 class Thread {
   static MaxTweetChars = 280;
+  static MaxTweetBlueChars = 4000;
 
   static CountTweets = 0;
 
@@ -31,9 +32,9 @@ class Thread {
     // get substr
     let tweet: string = (end === 0) ? text.substring(start) : text.substring(start, end);
     // get last tab space in the substring
-    if ((Thread.MaxTweetChars - (end - start)) >= option) return tweet;
+    if ((Thread.MaxTweetChars - (end - start)) > option) return tweet;
     let lastSpace: number = tweet.lastIndexOf(separator);
-    if (lastSpace === -1 || (end - lastSpace) > 15) return Thread.extractTweetText(text, start, end - option - 1, option);
+    if (lastSpace === -1 || (end - lastSpace) > 15) return Thread.extractTweetText(text, start, end - option , option);
     return Thread.extractTweetText(text, start, start + lastSpace, option);
   }
 
@@ -45,7 +46,7 @@ class Thread {
     let option: number = Thread.numbering.toString().length;
     for (let i = 0; i < threadLength; i++) {
       let tweet: string = '';
-      let end: number = (i + 1 === threadLength) ? 0 : charInThread + Thread.MaxTweetChars;
+      let end: number = (i + 1 === threadLength) ? 0 : (charInThread + Thread.MaxTweetChars - 1);
       tweet = Thread.extractTweetText(text, charInThread, end, option);
       thread.push(tweet);
       if (threadLength !== Thread.CountTweets) {
