dbbdd3a9171c4f0f69077b5d9bbcc6c9ef05ecf7
usbbush <jeanbienvenusb@gmail.com>
2023-02-25 00:47:00 +0200

    Display tweet Container  when text exist

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -18,21 +18,22 @@ function TweetComp({text}: props) {
   }
 
   return (
-    <div className="my-6 px-3 py-2 border w-full bg-white rounded relative mt-10">
+    text.length > 0 ? (<div className="my-6 px-3 py-2 border w-full bg-white rounded relative mt-10">
       <div className="absolute right-0 -top-9 p-1 border border-1 bg-white">
         <div className="flex items-center gap-2">
           <div className="msg text-blue-600 ml-auto flex-grow hidden ">Copied</div>
           <div className="order-">
             {text.length} / {Thread.MaxTweetChars}
           </div>
-          <button className=" shadow-lg p-1 border border-1 bg-blue-400 z-10 rounded" onClick={(e) => copyText(e, text)}>
+          <button className=" shadow-lg p-1 border border-1 bg-blue-400 z-10 rounded"
+                  onClick={(e) => copyText(e, text)}>
             <img src={copy} alt="copy icon" className="w-4 h-4"/>
           </button>
 
         </div>
       </div>
       <div style={{overflowWrap: 'break-word'}} className="text-gray-600 tweet">{text}</div>
-    </div>
+    </div>) : <></>
   );
 }
 
