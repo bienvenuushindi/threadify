88ff3cd0be2a30bb31cff502059e33460414cf98
usbbush <jeanbienvenusb@gmail.com>
2023-02-24 11:34:12 +0200

    Include logo

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import logo from '../images/logo.png';
 
-function Header(){
-  return(
+function Header() {
+  return (
     <header>
-      <div>Logo</div>
+      <div className="flex flex-col items-center justify-center p-2 mt-4">
+        <img src={logo} alt="threadify logo" className="w-h4 h-24"/>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
